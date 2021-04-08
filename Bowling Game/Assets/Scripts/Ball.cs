7474103798085f9806d2881c985ccddb56a08876
FacundoPonce <mrfacu125@gmail.com>
2021-04-07 22:38:28 -0300

    Scene and ball movement.

---
+++ Bowling Game/Assets/Scripts/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] float ballSpeed;
+    [SerializeField] Rigidbody ball;
+    [SerializeField] int forceMultipler;
+    [SerializeField] private float actualForce;
+
+    private bool throwBall = false;
+    private float forceAplied = 0;
+    public void MoveBall()
+    {
+        Vector3 moveVec = new Vector3(forceAplied, 0, 0);
+        //--
+        ball.AddForce(moveVec);
+    }
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            forceAplied += forceMultipler * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            forceAplied -= forceMultipler * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            MoveBall();
+        }
+        actualForce = forceAplied;
+    }
+}
---
+++ Bowling Game/Assets/Scripts/InitScenario.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitScenario : MonoBehaviour
+{
+    [SerializeField] GameObject prefabKegels;
+    [SerializeField] GameObject prefabBall;
+    [SerializeField] GameObject floor;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
